9acfb377ffd33589c1caa3e97bdcdaeaca60332a
Erica Droz <ericad2891@gmail.com>
2020-04-12 19:07:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-var greeting = "";
+var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "" + specialGuest + "!";
-
+;
 var topic = "space";
 
 var conversation = `${topic}`;
